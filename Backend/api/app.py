ea23aa19870d84ad30110ceb7faaa9b91e8217f8
Matheus Duncan <duncanmatheus905@gmail.com>
2023-09-16 11:49:53 -0400

    Added remaining DB tables.

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -25,9 +25,36 @@ def teams():
 def teamMembers():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT users.username, teams.name FROM userTeam  \
+            cur.execute("SELECT users.username AS USER, teams.name AS TEAM FROM userTeam  \
                         INNER JOIN teams ON userTeam.team = teams.id \
                         INNER JOIN users ON userTeam.teamMember = users.id \
+                        ;")
+            return cur.fetchall()
+        
+@app.route("/events")
+def events():
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM events")
+            return cur.fetchall()
+
+@app.route("/teamEvents")
+def teamEvents():
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT teams.name AS TEAM, events.name AS EVENT FROM teamEvent  \
+                        INNER JOIN teams ON teamEvent.team = teams.id \
+                        INNER JOIN events ON teamEvent.event = events.id \
+                        ;")
+            return cur.fetchall()
+        
+@app.route("/userEvents")
+def userEvents():
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT users.username AS USER, events.name AS EVENT FROM userEvent  \
+                        INNER JOIN users ON userEvent.teamMember = users.id \
+                        INNER JOIN events ON userEvent.event = events.id \
                         ;")
             return cur.fetchall()
 
