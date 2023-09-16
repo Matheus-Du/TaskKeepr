f45a73aa16e15b9877a12693c64002435143f6df
Matheus Duncan <duncanmatheus905@gmail.com>
2023-09-16 12:24:34 -0400

    Added endpoints for adding users and teams.

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import psycopg
 import os
 from dotenv import load_dotenv
@@ -7,19 +7,42 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/users")
+# TODO: Add routes for associating a user with a team, and a team/user with an event
+    # Change output of endpoints to be JSON
+
+@app.route("/users", methods=['GET', 'POST'])
 def users():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM users")
-            return cur.fetchall()
+            if request.method == 'GET':
+                cur.execute("SELECT * FROM users")
+                return cur.fetchall()
+            elif request.method == 'POST':
+                content = request.json
+                cur.execute("INSERT INTO users (id, username) VALUES ('{}', '{}');".format(content['id'], content['username']))
+                conn.commit()
+                return "User added"
+        
+@app.route("/users/<id>", methods=['GET'])
+def user(id):
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            if request.method == 'GET':
+                cur.execute("SELECT * FROM users WHERE id = '{}'".format(id))
+                return cur.fetchall()
 
-@app.route("/teams")
+@app.route("/teams", methods=['GET', 'POST'])
 def teams():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM teams")
-            return cur.fetchall()
+            if request.method == 'GET':
+                cur.execute("SELECT * FROM teams")
+                return cur.fetchall()
+            elif request.method == 'POST':
+                content = request.json
+                cur.execute("INSERT INTO teams (id, name) VALUES ('{}', '{}');".format(content['id'], content['name']))
+                conn.commit()
+                return "Team added"
 
 @app.route("/teamMembers")
 def teamMembers():
@@ -31,12 +54,19 @@ def teamMembers():
                         ;")
             return cur.fetchall()
         
-@app.route("/events")
+@app.route("/events", methods=['GET', 'POST'])
 def events():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM events")
-            return cur.fetchall()
+            if request.method == 'GET':
+                cur.execute("SELECT * FROM events")
+                return cur.fetchall()
+            elif request.method == 'POST':
+                content = request.json
+                cur.execute("INSERT INTO events (name, description, type, startDate, endDate, dateCreated) \
+                            VALUES ('{}', '{}', '{}', '{}', '{}', '{}');".format(content['name'], content['description'], content['type'], content['startDate'], content['endDate'], content['dateCreated']))
+                conn.commit()
+                return "Event added"
 
 @app.route("/teamEvents")
 def teamEvents():
