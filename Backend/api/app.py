21cf2ade078ee106fa28275f100fdb6a310bf85a
Owen Cooke <ocooke@ualberta.ca>
2023-09-16 03:24:36 -0400

    initial teams bot testing

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    try:
+        # Get the JSON data from the request
+        body = request.get_json()['data']
+        message = body['message']
+        print(message, datetime.datetime.now().isoformat())
+
+        # FIXME @LEO: chain route with Cohere API, using the `text`
+
+
+        # FIXME @MATHEUS: insert result from Cohere into DB
+
+        return jsonify({'message': 'Message summarized successfully'}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
