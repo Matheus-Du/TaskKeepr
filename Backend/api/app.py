fad81532b51478bcab59a432cb9b5a91030c7576
Matheus Duncan <duncanmatheus905@gmail.com>
2023-09-16 18:31:08 -0400

    Reworked endpoints for FE compatability.

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -8,15 +8,23 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# TODO: Add routes for associating a user with a team, and a team/user with an event
-    # Change output of endpoints to be JSON
+# TODO: Need to fix returns for GET requests; they should return obj ID's instead of names
 
+# Add a new user to the database or return all users
 @app.route("/users", methods=['GET', 'POST'])
-def users():
-    # Get all users or POST a new user
+def addUser():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            if request.method == 'GET':
+            if request.method == 'POST':
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO users (id, username) VALUES ('{}', '{}')".format(data['id'], data['username']))
+                    conn.commit()
+                except Exception:
+                    return "Error: User with that ID already exists", 500
+                else:
+                    return "Success"
+            elif request.method == 'GET':
                 cur.execute("SELECT * FROM users")
                 data = cur.fetchall()
                 # convert each row to a JSON object and return as a list of JSON objects
@@ -24,30 +32,22 @@ def users():
                 for row in data:
                     json.append({'id': row[0], 'username': row[1]})
                 return json
-            elif request.method == 'POST':
-                content = request.json
-                cur.execute("INSERT INTO users (id, username) VALUES ('{}', '{}');".format(content['id'], content['username']))
-                conn.commit()
-                return "User added"
         
-@app.route("/users/<id>", methods=['GET'])
-def user(id):
-    # Get specific user by id
-    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
-        with conn.cursor() as cur:
-            if request.method == 'GET':
-                cur.execute("SELECT * FROM users WHERE id = '{}'".format(id))
-                data = cur.fetchone()
-                # convert the row to a JSON object and return as a list of JSON objects
-                json = {'id': data[0], 'username': data[1]}
-                return json
-
+# Add a new team to the database or return all teams
 @app.route("/teams", methods=['GET', 'POST'])
-def teams():
-    # Get all teams or POST a new team
+def addTeam():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            if request.method == 'GET':
+            if request.method == 'POST':
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO teams (id, name) VALUES ('{}', '{}')".format(data['id'], data['name']))
+                    conn.commit()
+                except Exception:
+                    return "Error: Team with that ID already exists", 500
+                else:
+                    return "Success"
+            elif request.method == 'GET':
                 cur.execute("SELECT * FROM teams")
                 data = cur.fetchall()
                 # convert each row to a JSON object and return as a list of JSON objects
@@ -55,90 +55,107 @@ def teams():
                 for row in data:
                     json.append({'id': row[0], 'name': row[1]})
                 return json
-            elif request.method == 'POST':
-                content = request.json
-                cur.execute("INSERT INTO teams (id, name) VALUES ('{}', '{}');".format(content['id'], content['name']))
-                conn.commit()
-                return "Team added"
-
-@app.route("/teamMembers", methods=['GET'])
-def teamMembers():
-    # Get all users and their associated team
-    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT users.username AS USER, teams.name AS TEAM FROM userTeam  \
-                        INNER JOIN teams ON userTeam.team = teams.id \
-                        INNER JOIN users ON userTeam.teamMember = users.id \
-                        ;")
-            data = cur.fetchall()
-            # convert each row to a JSON object and return as a list of JSON objects
-            json = []
-            for row in data:
-                json.append({'teamMember': row[0], 'team': row[1]})
-            return json
-        
+                    
+# Add a new event to the database or return all events
 @app.route("/events", methods=['GET', 'POST'])
-def events():
-    # GET all events or POST a new event
+def addEvent():
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
-            if request.method == 'GET':
+            if request.method == 'POST':
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO events (name, description, type, startDate, endDate, dateCreated) VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(data['name'], data['description'], data['type'], data['startDate'], data['endDate'], data['dateCreated']))
+                    conn.commit()
+                except Exception:
+                    return "Error: Event with that ID already exists", 500
+                else:
+                    return "Success"
+            elif request.method == 'GET':
                 cur.execute("SELECT * FROM events")
                 data = cur.fetchall()
                 # convert each row to a JSON object and return as a list of JSON objects
                 json = []
                 for row in data:
-                    json.append({'id': row[0], 'name': row[1]})
+                    json.append({'id': row[0], 'name': row[1], 'description': row[2], 'type': row[3], 'startDate': row[4], 'endDate': row[5], 'dateCreated': row[6]})
                 return json
-            elif request.method == 'POST':
-                content = request.json
-                cur.execute("INSERT INTO events (name, description, type, startDate, endDate, dateCreated) \
-                            VALUES ('{}', '{}', '{}', '{}', '{}', '{}');".format(content['name'], content['description'], content['type'], content['startDate'], content['endDate'], content['dateCreated']))
-                conn.commit()
-                return "Event added"
-
-@app.route("/teamEvents", methods=['GET', 'POST'])
-def teamEvents():
-    # Get all teams and their associated events or POST a new team/event association
+            
+# Given a team ID, return all users on that team or POST a user to add to that team
+@app.route("/teams/<id>/users", methods=['GET', 'POST'])
+def teamUsers(id):
     with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
         with conn.cursor() as cur:
             if request.method == 'GET':
-                    cur.execute("SELECT events.name, teams.name FROM teamEvent  \
-                                INNER JOIN teams ON teamEvent.team = teams.id \
-                                INNER JOIN events ON teamEvent.event = events.id \
-                                ;")
-                    data = cur.fetchall()
-                    # convert each row to a JSON object and return as a list of JSON objects
-                    json = []
-                    for row in data:
-                        json.append({'event': row[0], 'team': row[1]})
-                    return json
-            elif request.method == 'POST':
-                    content = request.json
-                    cur.execute("INSERT INTO teamEvent (event, team) VALUES ('{}', '{}');".format(content['event'], content['team']))
-                    conn.commit()
-                    return "Team/Event association added"
-        
-@app.route("/userEvents", methods=['GET', 'POST'])
-def userEvents():
-    # Get all users and their associated events or POST a new user/event association
-    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
-        with conn.cursor() as cur:
-            if request.method == 'GET':
-                data = cur.execute("SELECT users.username AS USER, events.name AS EVENT FROM userEvent \
-                            INNER JOIN users ON userEvent.teamMember = users.id \
-                            INNER JOIN events ON userEvent.event = events.id \
-                            ;")
+                cur.execute("SELECT users.username FROM userTeam \
+                            INNER JOIN users ON userTeam.teamMember = users.id \
+                            WHERE userTeam.team = '{}'".format(id))
+                data = cur.fetchall()
                 # convert each row to a JSON object and return as a list of JSON objects
                 json = []
                 for row in data:
-                    json.append({'user': row[0], 'event': row[1]})
+                    json.append({'username': row[0]})
                 return json
             elif request.method == 'POST':
-                content = request.json
-                cur.execute("INSERT INTO userEvent (event, teamMember) VALUES ('{}', '{}');".format(content['event'], content['teamMember']))
-                conn.commit()
-                return "User/Event association added"
+                # add user to team
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO userTeam (team, teamMember) VALUES ('{}', '{}')".format(id, data['user']))
+                    conn.commit()
+                except Exception:
+                    return "Error: User is already a member of that team", 500
+                else:
+                    return "Success"    
+                    
+# Given a user ID, return all events that user is associated with or POST an event to associate with that user
+@app.route("/users/<id>/events", methods=['GET', 'POST'])
+def userEvents(id):
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            if request.method == 'GET':
+                cur.execute("SELECT * FROM events \
+                            INNER JOIN userEvent ON events.id = userEvent.event \
+                            WHERE userEvent.teamMember = '{}'".format(id))
+                data = cur.fetchall()
+                # convert each row to a JSON object and return as a list of JSON objects
+                json = []
+                for row in data:
+                    json.append({'event': row[0]})
+                return json
+            elif request.method == 'POST':
+                # assign event to user
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO userEvent (event, teamMember) VALUES ('{}', '{}')".format(data['event'], id))
+                    conn.commit()
+                except Exception:
+                    return "Error: Event is already associated with that user", 500
+                else:
+                    return "Success"
+                        
+# Given a team ID, return all events that team is associated with or POST an event to associate with that team
+@app.route("/teams/<id>/events", methods=['GET', 'POST'])
+def teamEvents(id):
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            if request.method == 'GET':
+                cur.execute("SELECT events.name FROM teamEvent \
+                            INNER JOIN events ON teamEvent.event = events.id \
+                            WHERE teamEvent.team = '{}'".format(id))
+                data = cur.fetchall()
+                # convert each row to a JSON object and return as a list of JSON objects
+                json = []
+                for row in data:
+                    json.append({'event': row[0]})
+                return json
+            elif request.method == 'POST':
+                # assign event to team
+                data = request.get_json()
+                try:
+                    cur.execute("INSERT INTO teamEvent (event, team) VALUES ('{}', '{}')".format(data['event'], id))
+                    conn.commit()
+                except Exception:
+                    return "Error: Event is already associated with that team", 500
+                else:
+                    return "Success"
 
 @app.route("/")
 def hello_world():
