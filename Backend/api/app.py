012f4abde3c4cf381ed1fa1980d591b583546123
Matheus Duncan <duncanmatheus905@gmail.com>
2023-09-16 11:13:51 -0400

    Added DB link between users and teams.

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -21,6 +21,16 @@ def teams():
             cur.execute("SELECT * FROM teams")
             return cur.fetchall()
 
+@app.route("/teamMembers")
+def teamMembers():
+    with psycopg.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT users.username, teams.name FROM userTeam  \
+                        INNER JOIN teams ON userTeam.team = teams.id \
+                        INNER JOIN users ON userTeam.teamMember = users.id \
+                        ;")
+            return cur.fetchall()
+
 @app.route("/")
 def hello_world():
     return "Hello World"
