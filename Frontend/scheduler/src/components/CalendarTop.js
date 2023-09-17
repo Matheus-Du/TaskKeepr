7382adaa230e4eaa3087f41b1e6d4c263ebbb9b5
Owen Cooke <ocooke@ualberta.ca>
2023-09-17 07:49:57 -0400

    final changes

--- Frontend/scheduler/src/components/CalendarTop.js
+++ Frontend/scheduler/src/components/CalendarTop.js
@@ -8,25 +8,25 @@ export const CalendarTop = () => {
         <p className="text-gray-100 text-center">Tasks</p>
       </div>
       <div className="w-7/8 flex">
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Sunday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Monday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Tuesday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Wednesday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Thursday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Friday</p>
         </div>
-        <div className="flex-1/7 grow flex items-center justify-center">
+        <div className="max-w-[155px] flex-1/7 grow flex items-center justify-center">
           <p className="text-gray-100">Saturday</p>
         </div>
       </div>
